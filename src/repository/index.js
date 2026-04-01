54ca5988820f0064953a3c6d508db3c8b9ab7489
Abhishek Nishad <codedaily04@gmail.com>
2026-04-01 21:36:30 +0530

    Added City-service

---
+++ src/repository/index.js
@@ -0,0 +1,5 @@
+// If we are using this const "variable name" manner then, we can directly excess the objects, else we have to use {} braces to get the attributes and variables.
+module.exports = {
+    CityRepository: require('./city-repository')
+}
+//We are using index.js just to accumulate all the repo of repository in one place, just to send all the repos to different from one file
