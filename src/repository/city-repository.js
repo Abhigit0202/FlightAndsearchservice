a9aa4b3850233e9a5172f37cd0f08dec96a3bb00
Abhishek Nishad <codedaily04@gmail.com>
2026-04-01 18:06:57 +0530

    Completed City Repo

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -3,26 +3,60 @@ const { City }=require('../models/index');
 
 
 class CityRepository{
+
     async createCity({name}){
         try {
-            const city = await City.create({name});
+            const city=await City.create({
+                name:name // first name represent the model property "name"
+            });
             return city;
         } catch (error){
-            throw {error}; 
+            console.log("Something went wrong");
+            throw {error};
         }
     }
 
-    async deleteCity(cityid){
-        try{
+    async deleteCity(cityId){
+        try {
             await City.destroy({
                 where:{
-                    id: cityid
+                    id : cityId
                 }
-            });
-        }catch(error){
-            throw {error}; 
+            })
+        } catch (error) {
+            console.log("Something went wrong");
+            throw {error};
         }
     }
+
+    async updateCity(cityId){
+        try {
+            const city= await City.update(data, {
+                where:{
+                    id:cityId
+                }
+            });
+            return city;
+        } catch (error) {
+            
+        }
+    }
+
+    async getCity(cityId){
+        try {
+            // const city=await City.findone({
+            //     where:{
+            //         id: cityId
+            //     }
+            // }) or
+            const city=await City.findbyPk(cityId);//above and this function is same
+            return city;
+        } catch (error) {
+            console.log("Something went wrong");
+            throw {error};
+        }
+    }
+
 }
 
 module.exports= CityRepository;
